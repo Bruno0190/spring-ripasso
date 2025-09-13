f91f86d7bf6f9afd18d8914e28603bc7ec083e9a
Bruno0190 <bruno.varadero90@gmail.com>
2025-09-13 16:15:27 +0200

    first commit

---
+++ src/main/java/org/esercizi_di_ripasso/java/spring/spring_ripasso/SpringRipassoApplication.java
@@ -0,0 +1,13 @@
+package org.esercizi_di_ripasso.java.spring.spring_ripasso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRipassoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRipassoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/esercizi_di_ripasso/java/spring/spring_ripasso/SpringRipassoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.esercizi_di_ripasso.java.spring.spring_ripasso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRipassoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
